a55fbeba632998a85278997e7d8146fb0744b9ad
林小龙 <xiaolong.lin@ele.me>
2017-12-12 13:50:19 +0800

    refactor: name subRules to matchedRules, element to component

--- utils/check-for-deprecations.js
+++ utils/check-for-deprecations.js
@@ -5,34 +5,34 @@ var path = require('path');
 var assertRule = require('./assert-rule');
 var reportWarning = require('./report-warning');
 
-var subRules = [];
+var matchedRules = [];
 var rulesPath = path.join(__dirname, '../rules');
 var rules = recursiveReaddirSync(rulesPath)
   .filter(function (file) {
     return file.indexOf('.js') === file.length - 3 && file.indexOf('.spec') === -1;
   })
   .reduce(function (preResult, file) {
-    var element = file.match(/.*\/(.*)\/.*$/)[1];
+    var component = file.match(/.*\/(.*)\/.*$/)[1];
     var rule = require(file);
     rule.file = file;
-    if (!preResult[element]) {
-      preResult[element] = [];
+    if (!preResult[component]) {
+      preResult[component] = [];
     }
-    preResult[element].push(rule);
+    preResult[component].push(rule);
     return preResult;
   }, {});
 
 module.exports = function(fileData) {
-  Object.keys(rules).some(function(element) {
-    var matches = fileData.line.match(new RegExp('<' + element));
+  Object.keys(rules).some(function(component) {
+    var matches = fileData.line.match(new RegExp('<' + component));
     if (matches) {
-      subRules = rules[element];
+      matchedRules = rules[component];
       return true;
     }
     return false;
   });
 
-  return subRules.some(function(rule) {
+  return matchedRules.some(function(rule) {
     var warning = assertRule(fileData, rule);
     if (warning) {
       reportWarning(fileData, warning, rule);
