0ac8cd71dd1ae0975104103c37db4e7a7dd6af1b
林小龙 <xiaolong.lin@ele.me>
2017-12-14 14:38:34 +0800

    feat: add more exmaples

---
+++ examples/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="template">
+    <el-dialog
+      v-model="true" size="large">
+      this is template body
+      <el-rate text-template="{ value }"></el-rate>
+      <el-dropdown menu-align="left"></el-dropdown>
+      <el-transfer
+        footer-format="{}"></el-transfer>
+      <el-switch on-text="ON"
+                 off-text="OFF"></el-switch>
+      <el-tag type="primary"></el-tag>
+      <el-tag type="gray"></el-tag>
+      <el-menu theme="dark"></el-menu>
+      <el-input icon="el-icon-infomation"
+                :on-icon-click="clickEvent" @click="clickEvent"
+                @change="inputEvent"></el-input>
+      <el-autocomplete custom-item="my-item" :props="{}"></el-autocomplete>
+      <el-steps :center="true"></el-steps>
+      <el-date-picker @change="changeEvent"></el-date-picker>
+      <el-table inline-template="inline"></el-table>
+    </el-dialog>
+  </div>
+</template>
+<script type="text/ecmascript-6" lang="babel">
+  export default {
+    data() {
+      return {
+      };
+    },
+    methods: {
+      clickEvent() {
+        console.log('clicked');
+      },
+      inputEvent() {
+        console.log('inputed');
+      }
+    }
+  };
+</script>
+<style lang="scss" type="text/scss">
+  .template {
+    background-color: #ff0000;
+  }
+</style>
